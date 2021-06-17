968807698cddeeed1717666d31ae4b166c2a0685
arifrudianzfordev <arifrudianzfordeveloper@gmail.com>
2021-06-17 16:23:58 +0700

    Latihan input baru di field

---
+++ textfieldwidget/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Text Field"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              TextField(
+                // obscureText: true,
+                // maxLength: 5, //kita hanya bisa menulis 5 karakter
+                onChanged: (value) {
+                  setState(() {});
+                },
+                controller: controller,
+              ),
+              Text(controller.text)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
